d83c88c3d98b1825f3b344d389f5223c19fb20ec
Guillermo Bereche <bere0058@algonquinlive.com>
2022-12-15 19:36:24 -0500

    s

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -55,26 +55,26 @@ const Cart = () => {
                 <h1>Ma commande</h1>
                 <section className="main-container">
                     <div className="d-flex flex-row justify-content-between py-3">
-                        <h4 className="t-color-2 col-5">Produits</h4>
-                        <h4 className="t-color-2 col-3">Prix</h4>
+                        <h4 className="text-start t-color-2 col-5">Produits</h4>
                         <h4 className="t-color-2 col-4">Quantité</h4>
+                        <h4 className="t-color-2 col-3">Prix</h4>
                     </div>
                     {cartData.map((dish) => (
                         <div className="d-flex flex-row justify-content-between py-2" id={dish.name + " added"}>
                             <p className="col-5 text-start" key={dish.name}>{dish.name}</p>
-                            <div className="d-flex col-3">
-                                <p className="ps-3 me-auto">{dish.price * dish.quantity}€</p>
-                            </div>
-                            <div className="d-flex col-4">
-                                <button className="fa-plus t-color-1 pe-2" onClick={() => addHandler(dish)} ></button>
+                            <div className="d-flex col-4 px-2">
+                                <button className="fa-plus t-color-1" onClick={() => addHandler(dish)} ></button>
                                 <p className="m-auto">{dish.quantity}</p>
-                                <button className="t-color-1 ps-2" onClick={() => sustractHandler(dish)}>-</button>
+                                <button className="t-color-1" onClick={() => sustractHandler(dish)}>-</button>
+                            </div>
+                            <div className="d-flex col-3">
+                                <p className="m-auto">{dish.price * dish.quantity} €</p>
                             </div>
                         </div>
                     ))}
-                    <div className="d-flex flex-row justify-content-between pt-4">
-                        <p className="t-color-2">Total</p>
-                        <p>{cartTotal}€</p>
+                    <div className="d-flex justify-content-between pt-4">
+                        <p className="text-start t-color-2 col-5">Total</p>
+                        <p className='col-3'>{cartTotal} €</p>
                     </div>
                 </section>
             </main>
